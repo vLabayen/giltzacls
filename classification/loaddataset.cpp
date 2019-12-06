60268bf58b74457d644a3608535ec352f51cb220
vLabayen <victor.mendi3216@gmail.com>
2019-12-06 14:24:17 +0100

    retocando tab loaddataset

--- classification/loaddataset.cpp
+++ classification/loaddataset.cpp
@@ -10,55 +10,88 @@ void LoadDataset::setup(){
 }
 
 void LoadDataset::loadDataset_onClick(){
+    //Disconnect para evitar que se llamen al modificar el contenido de los comboBox
     disconnect(parent->ui->foundClasses_comboBox, SIGNAL(currentIndexChanged(int)), this, SLOT(selectedClass_onChange(int)));
     disconnect(parent->ui->foundImages_comboBox, SIGNAL(currentIndexChanged(int)), this, SLOT(selectedImage_onChange(int)));
 
-    QString mainDir = parent->ui->datasetPath_lineEdit->text();
+    //Comprobamos que existe el directorio especificado
+    if (!dir_exists(parent->ui->datasetPath_lineEdit->text().toStdString().c_str())){
+        setError("El directorio no existe");
+        parent->ui->foundClasses_label->setText("Clases encontradas :");
+        parent->ui->foundImages_label->setText("Imágenes encontradas :");
+        return;
+    }
+    mainDir = parent->ui->datasetPath_lineEdit->text();
 
+    //Obtenemos las subcarpetas (clases) dentro del directorio
     int classes = get_dirs(mainDir.toStdString().c_str(), parent->ui->foundClasses_comboBox);
     parent->ui->foundClasses_label->setText(QString("Clases encontradas : %1").arg(QString::number(classes)));
+    if (classes == 0){
+        setError("No hay carpetas en el directorio");
+        parent->ui->foundImages_label->setText("Imágenes encontradas :");
+        return;
+    }
 
-    QString currentDir = QString("%1/%2").arg(mainDir, parent->ui->foundClasses_comboBox->currentData().toString());
+    //Obtenemos las imagenes en la subcarpeta que se seleccione
+    selectedDir = QString("%1/%2").arg(mainDir, parent->ui->foundClasses_comboBox->currentData().toString());
+    int images = get_files(selectedDir.toStdString().c_str(), parent->ui->foundImages_comboBox);
+    parent->ui->foundImages_label->setText(QString("Imágenes encontradas : %1").arg(QString::number(images)));
 
-    int images = get_files(currentDir.toStdString().c_str(), parent->ui->foundImages_comboBox);
-    parent->ui->foundImages_label->setText(QString("Imagenes encontradas : %1").arg(QString::number(images)));
+    //De haber alguna imagen la mostramos, en caso contrario limpiamos la posible imagen que se este mostrando
+    if (images > 0) previewImage(parent->ui->foundImages_comboBox->currentIndex());
+    else parent->ui->imageDisplay_label->clear();
 
-    previewImage(parent->ui->foundClasses_comboBox->currentIndex(), parent->ui->foundImages_comboBox->currentIndex());
-
+    //Reconectamos los comboBox
     connect(parent->ui->foundClasses_comboBox, SIGNAL(currentIndexChanged(int)), this, SLOT(selectedClass_onChange(int)));
     connect(parent->ui->foundImages_comboBox, SIGNAL(currentIndexChanged(int)), this, SLOT(selectedImage_onChange(int)));
 }
 
 void LoadDataset::selectedClass_onChange(int index){
+    //Disconect del comboBox de las imagenes para que no se llame al modificar su contenido
     disconnect(parent->ui->foundImages_comboBox, SIGNAL(currentIndexChanged(int)), this, SLOT(selectedImage_onChange(int)));
 
-    QString currentDir = QString("%1/%2").arg(parent->ui->datasetPath_lineEdit->text(), parent->ui->foundClasses_comboBox->itemData(index).toString());
+    //Obtenemos el directorio que se ha seleccionado y las imagenes dentro de el
+    selectedDir = QString("%1/%2").arg(mainDir, parent->ui->foundClasses_comboBox->itemData(index).toString());
+    int images = get_files(selectedDir.toStdString().c_str(), parent->ui->foundImages_comboBox);
+    parent->ui->foundImages_label->setText(QString("Imágenes encontradas : %1").arg(QString::number(images)));
 
-    int images = get_files(currentDir.toStdString().c_str(), parent->ui->foundImages_comboBox);
-    parent->ui->foundImages_label->setText(QString("Imagenes encontradas : %1").arg(QString::number(images)));
+    //De haber alguna imagen la mostramos, en caso contrario limpiamos la posible imagen que se este mostrando
+    if (images > 0) previewImage(parent->ui->foundImages_comboBox->currentIndex());
+    else parent->ui->imageDisplay_label->clear();
 
-    previewImage(index, parent->ui->foundImages_comboBox->currentIndex());
-
+    //Reconectamos el comboBox
     connect(parent->ui->foundImages_comboBox, SIGNAL(currentIndexChanged(int)), this, SLOT(selectedImage_onChange(int)));
 }
 
 void LoadDataset::selectedImage_onChange(int index){
-    previewImage(parent->ui->foundClasses_comboBox->currentIndex(), index);
+    //Mostramos la imagen
+    previewImage(index);
 }
 
-void LoadDataset::previewImage(const int classIndex, const int imageIndex){
-    QString selectedImagePath = QString("%1/%2/%3").arg(
-                parent->ui->datasetPath_lineEdit->text(),
-                parent->ui->foundClasses_comboBox->itemData(classIndex).toString(),
-                parent->ui->foundImages_comboBox->itemData(imageIndex).toString()
-    );
+void LoadDataset::previewImage(const int imageIndex){
+    //Leemos la imagen seleccionada en formato RGB
+    cvtColor(cv::imread(QString("%1/%2").arg(selectedDir, parent->ui->foundImages_comboBox->itemData(imageIndex).toString()).toStdString()), selectedImage, CV_BGR2RGB);
 
-    selectedImage = cv::imread(selectedImagePath.toStdString());
-    cvtColor(selectedImage, selectedImage, CV_BGR2RGB);
+    //Mostramos la imagen en el label
+    parent->ui->imageDisplay_label->setPixmap(QPixmap::fromImage(QImage((const unsigned char*) (selectedImage.data), selectedImage.cols, selectedImage.rows, QImage::Format_RGB888)));
 
-    QImage qt_image = QImage((const unsigned char*) (selectedImage.data), selectedImage.cols, selectedImage.rows, QImage::Format_RGB888);
-    parent->ui->imageDisplay_label->setPixmap(QPixmap::fromImage(qt_image));
-
+    //Mostramos la informacion sobre la resolución de la imagen
     parent->ui->imageInfoRows_label->setText(QString("Filas : %1").arg(QString::number(selectedImage.rows)));
     parent->ui->imageInfoCols_label->setText(QString("Columnas : %1").arg(QString::number(selectedImage.cols)));
+}
+
+void LoadDataset::setError(const char* msg){
+    //Escribimos el error y establecemos un timer para eliminarlo en "errorTimeout"
+    parent->ui->loadDataset_loadError_label->setText(msg);
+    QTimer::singleShot(errorTimeout, this, SLOT(removeError()));
+
+    //Limpiamos la posible imagen que se este mostrando y el contenido de los comboBox
+    parent->ui->imageDisplay_label->clear();
+    parent->ui->foundClasses_comboBox->clear();
+    parent->ui->foundImages_comboBox->clear();
+}
+
+void LoadDataset::removeError(){
+    //Eliminamos el error
+    parent->ui->loadDataset_loadError_label->setText("");
 }
--- classification/loaddataset.h
+++ classification/loaddataset.h
@@ -15,18 +15,24 @@ public:
     ~LoadDataset();
 
     void setup(void);
-    void previewImage(int classIndex, int imageIndex);
+    void previewImage(int imageIndex);
+    void setError(const char* msg);
 
+    QString mainDir;
+    QString selectedDir;
     cv::Mat selectedImage;
 
 private:
     MainWindow* parent;
+
+    int errorTimeout = 5000;
 
 private slots:
     void loadDataset_onClick(void);
     void selectedClass_onChange(int index);
     void selectedImage_onChange(int index);
 
+    void removeError(void);
 };
 
 #endif // LOADDATASET_H
